f8024874ab17550d07eaf3e95e950f3322b3f941
Ruben Saragih <ruben.saragih@livecom.com>
2025-04-09 22:36:02 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ const lightTheme = themes.github;
 module.exports = {
   title: "MyWiki",
   trailingSlash: false,
-  url: "https://zealous-dune-02dd8c700.6.azurestaticapps.net",
+  url: "https://white-pebble-0a8b06200.6.azurestaticapps.net",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
