af029d830cf69dfd000c3293f7a3b83944b5b18e
saragihruben <ruben.saragih@outlook.com>
2025-04-10 14:56:01 +0700

    azure web static

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,4 +1,5 @@
 export default [
+  require("/Users/saragihruben/Documents/GitHub/my-azure-web-static/node_modules/@docusaurus/plugin-google-gtag/lib/gtag"),
   require("/Users/saragihruben/Documents/GitHub/my-azure-web-static/node_modules/infima/dist/css/default/default.css"),
   require("/Users/saragihruben/Documents/GitHub/my-azure-web-static/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
   require("/Users/saragihruben/Documents/GitHub/my-azure-web-static/node_modules/@docusaurus/theme-classic/lib/nprogress"),
