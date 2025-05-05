d38968c5c42a2127bb239f8e7b90e48c95421158
saragihruben <ruben.saragih@outlook.com>
2025-05-05 14:59:17 +0700

    azure blob

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,6 +1,6 @@
 export default [
-  require("Z:\\azure\\docusaurus\\github\\my-azure-web-static\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("Z:\\azure\\docusaurus\\github\\my-azure-web-static\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("Z:\\azure\\docusaurus\\github\\my-azure-web-static\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("Z:\\azure\\docusaurus\\github\\my-azure-web-static\\src\\css\\custom.css"),
+  require("/Users/saragihruben/Documents/GitHub/my-azure-web-static/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/saragihruben/Documents/GitHub/my-azure-web-static/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/saragihruben/Documents/GitHub/my-azure-web-static/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/saragihruben/Documents/GitHub/my-azure-web-static/src/css/custom.css"),
 ];
