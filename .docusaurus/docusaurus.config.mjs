b34a9f90b82f36c2f4c6d7deedb8cd49ccd378e7
Ruben Saragih <ruben.saragih@livecom.com>
2025-05-05 20:59:19 +0700

    azure storage security

--- .docusaurus/docusaurus.config.mjs
+++ .docusaurus/docusaurus.config.mjs
@@ -187,7 +187,7 @@ export default {
       {
         "docs": {},
         "theme": {
-          "customCss": "/Users/saragihruben/Documents/GitHub/my-azure-web-static/src/css/custom.css"
+          "customCss": "Z:\\azure\\docusaurus\\github\\my-azure-web-static\\src\\css\\custom.css"
         },
         "gtag": {
           "trackingID": "G-Q1MDM8KFVG"
