e04de4329445166ef5ef1d1dc291fcb6a8a7979c
Ruben Saragih <ruben.saragih@livecom.com>
2025-05-31 23:14:29 +0700

    az monitor-and-backup-resources

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -68,9 +68,9 @@ function Async() {
           <div className="col col--6">
             <br/>
             <p className={styles.paddingTopLg}>
-              Beberapa hands-On dan penjelasan materi pada Wiki akan dilengkapi dengan video dan di harapkan dapat mempermudah pemahaman tentang materi yang akan di pelajari.
+              Some hands-on sessions and material explanations on the Wiki will be enhanced with videos and are expected to help make it easier to understand the material being studied.
               <br/>
-              Subscribe channel youtube <a href="https://www.youtube.com/@SaragihRubens" target="_blank">Saragih Ruben</a> untuk mendukung saya dalam pembuatan materi dan video selanjutnya.
+              Subscribe to <a href="https://www.youtube.com/@SaragihRubens" target="_blank">Saragih Ruben</a> YouTube channel to support me in creating future materials and videos.
             </p>
           </div>
 
